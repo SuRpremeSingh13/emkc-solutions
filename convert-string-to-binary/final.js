b0ac70a307850c281ed36a821f66e028c18b1c80
Surpreet Singh <64738087+SuRpremeSingh13@users.noreply.github.com>
2022-05-13 21:14:03 -0400

    Update final.js

--- convert-string-to-binary/final.js
+++ convert-string-to-binary/final.js
@@ -1,5 +1,5 @@
 const value1 = process.argv[2];
-
+//thanks to @ziloka
 // write your solution here
 let binaryV = value1.split('').map(x => '0' + x.charCodeAt(0).toString(2)).join('')
 console.log(binaryV)
